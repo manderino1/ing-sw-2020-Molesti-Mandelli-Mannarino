280ab8dbf43d0cc8e4c24ddd5675c911daa26431
Unknown <marco.mandelli212@gmail.com>
2020-03-25 15:08:11 +0100

    Added PlayerManager basic methods

--- src/main/java/it/polimi/ingsw/PSP18/controller/PlayerManager.java
+++ src/main/java/it/polimi/ingsw/PSP18/controller/PlayerManager.java
@@ -1,24 +1,48 @@
 package it.polimi.ingsw.PSP18.controller;
 
 import it.polimi.ingsw.PSP18.controller.divinities.Divinity;
-import it.polimi.ingsw.PSP18.model.PlayerData;
-import it.polimi.ingsw.PSP18.model.Map;
-import it.polimi.ingsw.PSP18.model.Worker;
+import it.polimi.ingsw.PSP18.model.*;
 
 public class PlayerManager {
     private Map map;
-    private Worker[] worker = new Worker[2];
+    private Worker[] workers = new Worker[2];
     private PlayerData playerData;
     private Divinity divinity;
 
     public PlayerManager() {
     }
 
-    public Worker[] getWorker() {
-        return worker;
+    /***
+     *
+     * @param workerID the index of the worker to return (0 or 1)
+     * @return a copy of the selected worker
+     */
+    public Worker getWorker(Integer workerID) {
+        return new Worker(workers[workerID].getX(), workers[workerID].getY());
     }
 
-    public void setWorker(Worker[] worker) {
-        this.worker = worker;
+    /***
+     *
+     * @param oldX the old position of the worker on the x axis
+     * @param oldY the old position of the worker on the y axis
+     * @param direction the direction of the move
+     */
+    public void setMove(Integer oldX, Integer oldY, Direction direction) {
+    }
+
+    /***
+     * TODO: return a copy and not the actual MAP
+     * @return a copy of the map
+     */
+    public Map getMap() {
+        return map;
+    }
+
+    /***
+     *
+     * @param raiseForbidden true if Athena moved up and so you cannot move up in the turn
+     */
+    public void manageTurn(boolean raiseForbidden) {
+        divinity.manageTurn(raiseForbidden);
     }
 }
