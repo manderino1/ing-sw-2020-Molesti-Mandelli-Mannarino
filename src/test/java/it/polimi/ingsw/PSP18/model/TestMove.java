0fbf9f5969d655a7a15c57453a88e92cd5dd0322
Unknown <marco.mandelli212@gmail.com>
2020-03-28 18:23:38 +0100

    Junit added to POM and first test

---
+++ src/test/java/it/polimi/ingsw/PSP18/model/TestMove.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.PSP18.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMove {
+    // Test for checking the creation of the class
+    @Test
+    public void testMoveCreation() {
+        Move move = new Move(Direction.UP, 0);
+        Assert.assertEquals(Direction.UP, move.getDirection());
+        Assert.assertEquals(Integer.valueOf(0), move.getLevel());
+    }
+}
