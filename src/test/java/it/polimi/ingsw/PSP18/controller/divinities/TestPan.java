580dc4cf2c332e5b2ff8094f470d44c573b2c722
Alessandro <10581232@polimi.it>
2020-04-03 16:14:51 +0200

    Created test Pan

--- src/test/java/it/polimi/ingsw/PSP18/controller/divinities/TestPan.java
+++ src/test/java/it/polimi/ingsw/PSP18/controller/divinities/TestPan.java
@@ -15,20 +15,8 @@ public class TestPan {
     public void createPlayerManager() {playerManager = new PlayerManager(new GameMap(), new PlayerData("Test1", Color.RED, 0), "Pan");}
 
     @Test
-    public void TestCheckForVictory(){
-        Divinity divinity = new Divinity("Pan", playerManager);
-        playerManager.placeWorker(2,1);
-        playerManager.placeWorker(3,2);
-        playerManager.setBuild(2,1);
-        playerManager.setBuild(2,1);
-        Assert.assertEquals(playerManager.getGameMap().getCell(2,1).getBuilding(), Integer.valueOf(2));
-        Assert.assertEquals(divinity.checkForVictory(false), false);
-        divinity.setMove(2,1, Direction.UP);
-        Assert.assertEquals(divinity.checkForVictory(false), true);
-        divinity.setMove(2,2, Direction.UP);
-        playerManager.setBuild(3,2);
-        playerManager.setBuild(3,2);
-        playerManager.setBuild(3,2);
-        Assert.assertEquals(divinity.checkForVictory(true), true);
+    public void TestPanCounstructor(){
+        Pan divinity = new Pan("Pan", playerManager);
+        Assert.assertEquals(playerManager.getDivinityName(), "Pan");
     }
 }
