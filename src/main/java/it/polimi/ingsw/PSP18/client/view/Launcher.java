1793ef10b92011baface1f228bf0e163658326ed
8mann <8altair@gmail.com>
2020-04-11 16:19:15 +0200

    Client launcher

--- src/main/java/it/polimi/ingsw/PSP18/client/view/Launcher.java
+++ src/main/java/it/polimi/ingsw/PSP18/client/view/Launcher.java
@@ -1,4 +1,26 @@
 package it.polimi.ingsw.PSP18.client.view;
 
+import it.polimi.ingsw.PSP18.client.view.cli.CliViewUpdate;
+import it.polimi.ingsw.PSP18.networking.SocketClient;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Launcher {
+
+    private final int PORT = 9002;
+    private final InetAddress HOST = InetAddress.getLocalHost();
+
+    Launcher() throws IOException {
+        ViewUpdate cliviewupdate = new CliViewUpdate();
+
+
+        Socket clientsocket = new Socket(HOST, PORT);
+
+        SocketClient NewThread = new SocketClient (clientsocket, cliviewupdate);
+        NewThread.start();
+    }
+
 }
