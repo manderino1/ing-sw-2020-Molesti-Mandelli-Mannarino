072fc2403386a317c4d52fb48b1bec2be9db88d5
8mann <8altair@gmail.com>
2020-04-17 14:58:44 +0200

    divinities fix

--- src/main/java/it/polimi/ingsw/PSP18/server/controller/divinities/Demeter.java
+++ src/main/java/it/polimi/ingsw/PSP18/server/controller/divinities/Demeter.java
@@ -1,10 +1,7 @@
 package it.polimi.ingsw.PSP18.server.controller.divinities;
 
 import it.polimi.ingsw.PSP18.networking.SocketThread;
-import it.polimi.ingsw.PSP18.networking.messages.toclient.BuildList;
-import it.polimi.ingsw.PSP18.networking.messages.toclient.BuildListFlag;
-import it.polimi.ingsw.PSP18.networking.messages.toclient.MatchLost;
-import it.polimi.ingsw.PSP18.networking.messages.toclient.SingleMoveList;
+import it.polimi.ingsw.PSP18.networking.messages.toclient.*;
 import it.polimi.ingsw.PSP18.server.controller.DirectionManagement;
 import it.polimi.ingsw.PSP18.server.controller.PlayerManager;
 import it.polimi.ingsw.PSP18.server.model.Direction;
@@ -24,17 +21,25 @@ public class Demeter extends Divinity {
      */
     @Override
     protected void build() {
-        if (checkForLose(true, false)) {
+
+        if (checkForLose(raiseForbidden, false)) {
             for(SocketThread socket : playerManager.getMatch().getSockets()) {
                 socket.sendMessage(new MatchLost(playerManager.getPlayerData().getPlayerID()));
-                playerManager.getMatch().getPlayerManagers().remove(playerManager.getMatch().getCurrentPlayer());
+            }
 
-                Integer x1 = playerManager.getWorker(0).getX();
-                Integer y1 = playerManager.getWorker(0).getY();
-                Integer x2 = playerManager.getWorker(1).getX();
-                Integer y2 = playerManager.getWorker(1).getY();
-                playerManager.getGameMap().setCell(x1, y1, playerManager.getGameMap().getCell( x1, y1).getBuilding(), null);
-                playerManager.getGameMap().setCell(x2, y2, playerManager.getGameMap().getCell( x2, y2).getBuilding(), null);
+            playerManager.getMatch().getPlayerManagers().remove(playerManager.getMatch().getCurrentPlayer());
+
+            Integer x1 = playerManager.getWorker(0).getX();
+            Integer y1 = playerManager.getWorker(0).getY();
+            Integer x2 = playerManager.getWorker(1).getX();
+            Integer y2 = playerManager.getWorker(1).getY();
+            playerManager.getGameMap().setCell(x1, y1, playerManager.getGameMap().getCell( x1, y1).getBuilding(), null);
+            playerManager.getGameMap().setCell(x2, y2, playerManager.getGameMap().getCell( x2, y2).getBuilding(), null);
+
+            if(playerManager.getMatch().getPlayerManagers().size() == 1) {
+                for(SocketThread socket : playerManager.getMatch().getSockets()) {
+                    socket.sendMessage(new MatchWon(playerManager.getMatch().getPlayerManagers().get(0).getPlayerData().getPlayerID()));
+                }
             }
         }
 
