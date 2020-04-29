b85c18fab412c3fd22ac8cbd3527066bf934ab44
Unknown <marco.mandelli212@gmail.com>
2020-04-29 15:34:53 +0200

    Scenes package init

---
+++ src/main/java/it/polimi/ingsw/PSP18/client/view/gui/GuiViewUpdate.java
@@ -0,0 +1,92 @@
+package it.polimi.ingsw.PSP18.client.view.gui;
+
+import it.polimi.ingsw.PSP18.client.view.ViewUpdate;
+import it.polimi.ingsw.PSP18.networking.messages.toclient.*;
+
+public class GuiViewUpdate extends ViewUpdate {
+    public GuiViewUpdate() {
+    }
+
+    /*
+        From here we place the functions that are called when message of the selected types are read from socket
+         */
+    @Override
+    public void updateMap(GameMapUpdate gameMapUpdate) {
+
+    }
+
+    @Override
+    public void moveUpdate(MoveList movelist) {
+
+    }
+
+    @Override
+    public void updatePlayerData(PlayerDataUpdate playerDataUpdate) {
+
+    }
+
+    @Override
+    public void selectNick() {
+
+    }
+
+    @Override
+    public void selectDivinity(DivinityList divinityList) {
+
+    }
+
+    @Override
+    public void buildUpdate(BuildList buildList) {
+
+    }
+
+    @Override
+    public void matchLostUpdate(MatchLost matchLost) {
+
+    }
+
+    @Override
+    public void matchWonUpdate(MatchWon matchWon) {
+
+    }
+
+    @Override
+    public void startMatch(StartMatch startMatch) {
+
+    }
+
+    @Override
+    public void matchReadyUpdate(MatchReady matchReady) {
+
+    }
+
+    @Override
+    public void setWorker(PlaceReady placeReady) {
+
+    }
+
+    @Override
+    public void prometheusBuildListUpdate(PrometheusBuildList prometheusBuildList) {
+
+    }
+
+    @Override
+    public void singleMoveUpdate(SingleMoveList singleMoveList) {
+
+    }
+
+    @Override
+    public void buildListFlagUpdate(BuildListFlag buildListFlag) {
+
+    }
+
+    @Override
+    public void endTurn(EndTurnAvaiable endTurnAvaiable) {
+
+    }
+
+    @Override
+    public void atlasBuildUpdate(AtlasBuildList atlasBuildList) {
+
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/PSP18/client/view/gui/scenes/ConnectionController.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.PSP18.client.view.gui.scenes;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ConnectionController extends Controller {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        super.initialize(location, resources);
+    }
+}
