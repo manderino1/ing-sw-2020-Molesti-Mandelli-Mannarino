9a38271ed944f92763e3f73394fe880ff55574fa
Unknown <marco.mandelli212@gmail.com>
2020-05-26 18:46:17 +0200

    Removed debug multi clients

--- src/main/java/it/polimi/ingsw/PSP18/client/debug/Client2.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.PSP18.client.debug;
-
-import it.polimi.ingsw.PSP18.client.view.Launcher;
-
-public class Client2 {
-    public static void main(String[] args) {
-        Launcher launcher = new Launcher("127.0.0.1");
-    }
-}
